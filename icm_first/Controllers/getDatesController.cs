33f53b831d6cf3345c78c4cc10cfba6f10df4e16
OmarOdaini <ona11@msstate.edu>
2018-07-12 08:04:14 -0500

    run it

---
+++ icm_first/Controllers/getDatesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace icm_first.Controllers
+{
+    public class getDatesController : Controller
+    {
+
+    }
+}
