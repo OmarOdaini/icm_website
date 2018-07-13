c5cea8f6b536fa1f1f395995d2338f5be24f7329
OmarOdaini <ona11@msstate.edu>
2018-07-13 07:59:47 -0500

    7/13/2018: db connection in progress

--- icm_first/Scripts/Jsaextra.js
+++
@@ -1,5 +0,0 @@
-﻿// SideNav Button Initialization
-$(".button-collapse").sideNav();
-// SideNav Scrollbar Initialization
-var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-Ps.initialize(sideNavScrollbar);
